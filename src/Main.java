68a322dc24143c72c6cf059a537d085634ff24e0
Piret Spitsõn <sppiret@gmail.com>
2016-10-12 15:54:55 +0300

    Esimene commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Alustame pommitamusega!");
+	System.out.println("Autor: Piret");
+	System.out.println("Aasta: 2016");
+    }
+}
